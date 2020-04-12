308e6b4f0a78b60204a07dcb4fc6efd77143c1ec
abrahamlaria <abraham@abrahamlaria.com>
2020-04-11 21:16:35 -0700

    Seeded database and added migrations. Adeded Dtos and Automapper. Added new repository and User controller

---
+++ DatingApp.Api/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.Api.Models;
+
+namespace DatingApp.Api.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, Dtos.UserForListDto>()
+                .ForMember(dest => dest.PhotoUrl, opt =>
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => 
+                    opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<User, Dtos.UserForDetailedDto>()
+                .ForMember(dest => dest.PhotoUrl, opt =>
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => 
+                    opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, Dtos.PhotosForDetailedDto>();
+        }
+    }
+}
